05261b74f2820731e6dc47778c3904c95808b91d
Alex <al_exss@hotmail.com>
2021-01-13 11:16:10 -0300

    Post-Created, Header, Post-List

---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent{
+  posts = [
+    { title: 'My First Post', content: 'This is my first post\'s content' },
+    { title: 'My Second Post', content: 'This is my second post\'s content' },
+    { title: 'My Third Post', content: 'This is my third post\'s content' },
+  ]
+}
